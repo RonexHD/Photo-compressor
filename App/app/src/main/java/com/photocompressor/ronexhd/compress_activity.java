4633695bf3917c7c9bb5cd8f80164c1af84777e2
RonexHD <bsc-inf-06-18@unima.ac.mw>
2022-10-18 21:38:58 +0200

    Added ui elements

---
+++ App/app/src/main/java/com/photocompressor/ronexhd/compress_activity.java
@@ -0,0 +1,14 @@
+package com.photocompressor.ronexhd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class compress_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compress);
+    }
+}
