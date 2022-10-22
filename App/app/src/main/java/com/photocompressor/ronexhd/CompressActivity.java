b0fb5452407fa5c8567bfb5b7c02b47b43deb0db
RonexHD <bsc-inf-06-18@unima.ac.mw>
2022-10-22 23:58:42 +0200

    Added View

--- App/app/src/main/java/com/photocompressor/ronexhd/CompressActivity.java
+++ App/app/src/main/java/com/photocompressor/ronexhd/CompressActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class compress_activity extends AppCompatActivity {
+public class CompressActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- App/app/src/main/java/com/photocompressor/ronexhd/MainActivity.java
+++ App/app/src/main/java/com/photocompressor/ronexhd/MainActivity.java
@@ -3,21 +3,66 @@ package com.photocompressor.ronexhd;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Toolbar mToolbar;
 
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mToolbar = findViewById(R.id.main_toolbar);
+        button = findViewById(R.id.select_image_btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCompressActivity();
+            }
+        });
+
+
+        Toolbar mToolbar = findViewById(R.id.main_toolbar);
         setSupportActionBar(mToolbar);
+        
+        askPermission();
+    }
+
+    private void openCompressActivity() {
+        Intent intent =new Intent(this, CompressActivity.class);
+        startActivity(intent);
+    }
+
+    private void askPermission() {
+
+        Dexter.withContext(this)
+                .withPermissions(android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                        android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport multiplePermissionsReport) {
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> list, PermissionToken permissionToken) {
+                        permissionToken.continuePermissionRequest();
+                    }
+                }).check();
     }
 
     @Override
